4d1eefcce95c5feabc41c40ed866bc06074da795
estagent <estagent@chip.me>
2020-12-28 01:18:49 +0300

    loader updated with _setOrThrowIfExists

--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,9 @@
+const _setOrThrowExists = (target, key, value) => {
+    if (typeof target[key] !== undefined)
+        throw `${key} already exists `
+    target[key] = value;
+}
+
 export default (target) => {
     target['load'] = function (mixed) {
         if (arguments.length > 1)
@@ -5,8 +11,12 @@ export default (target) => {
         else if (Array.isArray(mixed))
             for (let element of mixed) this.load(element)
         else if (typeof mixed === 'object')
-            for (let key of Object.keys(mixed)) this[key] = mixed[key]
-        else if (typeof mixed === 'function') this[mixed.name] = mixed
+            for (let key of Object.keys(mixed))
+                _setOrThrowExists(this, key, mixed[key]);
+
+        else if (typeof mixed === 'function')
+            _setOrThrowExists(this, mixed.name, mixed);
+        else throw "try to load as object to load undefined value"
 
         return this
     }
